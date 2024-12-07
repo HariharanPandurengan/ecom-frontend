da1255f3adb32ad4c764f9faf30130187dccf65a
Akro16 <akashganeshaner@gmail.com>
2024-12-07 22:22:01 +0530

    Design updated
    Design updated, filters needed to be optimized

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,14 @@
 import axios from "axios";
 import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
 import "../App.css";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { User, Heart, ShoppingBag } from 'lucide-react';
+
+
+
 interface SizesOptions {
     [key: string]: { [key: string]: string };
 }
@@ -39,24 +47,45 @@ interface Product {
 const Home: React.FC = () => {
     const [product, setProduct] = useState<Product>(initialProductState);
     const [products, setProducts] = useState([]);
+    const [sortedProducts, setSortedProducts] = useState([]);
+    const [countedProducts, setCountedProducts] = useState(products);
     const [sizes, setSizes] = useState(['S', 'M', 'L', 'XL'])
     const [materials, setMaterials] = useState(['Cotton', 'Nylon', 'Rexin'])
     const [prices, setPrices] = useState(['<500<', '<1000<', '<2000<', '<3000<'])
     const [colors, setColors] = useState(["Red", "Blue", "Green", "Black", "White", "Yellow", "Pink", "Purple", "Orange", "Brown", "Grey", "Navy", "Teal", "Maroon"])
+    const [isSticky, setIsSticky] = useState(false)
+    const [bagItemCount, setBagItemCount] = useState(0)
 
-    const fields = [
-        { name: 'name', label: 'Product Name', type: 'text' },
-        { name: 'price', label: 'Product Price', type: 'text' },
-        { name: 'category', label: 'Product Category', type: 'text' },
-        { name: 'subCategory', label: 'Product Sub Category', type: 'text' },
-        { name: 'material', label: 'Product Material', type: 'text' },
-        { name: 'totalQuantity', label: 'Product Total Quantity', type: 'text' },
-        { name: 'description', label: 'Product Description', type: 'textarea' },
-        { name: 'image', label: 'Product Image', type: 'file' },
-        { name: 'colors', label: 'Colors Available', type: 'select', options: ["Red", "Blue", "Green", "Black", "White", "Yellow", "Pink", "Purple", "Orange", "Brown", "Grey", "Navy", "Teal", "Maroon"], multiple: true },
-    ];
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+    };
+
     useEffect(() => {
         fetchProducts()
+
+    }, [])
+
+    useEffect(() => {
+        setBagItemCount(3) // Example: 3 items in the bag
+    }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsSticky(window.scrollY > 0)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
     }, [])
 
     const fetchProducts = () => {
@@ -64,6 +93,8 @@ const Home: React.FC = () => {
             .then(res => {
                 if (res.data.status === true) {
                     setProducts(res.data.products);
+                    setCountedProducts(res.data.products);
+                    setSortedProducts(res.data.products);
                     console.log(res.data.products)
                 }
             })
@@ -76,7 +107,7 @@ const Home: React.FC = () => {
 
     const fetchCountforFiltersbyMaterial = (material: string) => {
         var countforMaterial = 0;
-        products.forEach(prod => {
+        countedProducts.forEach(prod => {
             if (prod.material == material) {
                 countforMaterial = countforMaterial + parseInt(prod.totalQuantity)
             }
@@ -89,7 +120,7 @@ const Home: React.FC = () => {
         var countforprice = 0;
         price = price.slice(1, length - 1)
         //console.log(price);
-        products.forEach(prod => {
+        countedProducts.forEach(prod => {
             if (parseInt(prod.price) <= parseInt(price)) {
                 countforprice = countforprice + parseInt(prod.totalQuantity)
             }
@@ -100,7 +131,7 @@ const Home: React.FC = () => {
 
     const fetchCountforFiltersbyColor = (color: string) => {
         var countforColors = 0;
-        products.forEach(item => {
+        countedProducts.forEach(item => {
             for (const size in item.sizes[color]) {
                 //console.log("Size Count : ", color);
                 console.log("Size Count : ", item.sizes[color][size]);
@@ -115,7 +146,7 @@ const Home: React.FC = () => {
         let totalCount = 0;
 
         // Loop through each item to check if the size exists in any color
-        products.forEach(item => {
+        countedProducts.forEach(item => {
             for (const color in item.sizes) {
                 //console.log("Size Count : ", color);
                 if (item.sizes[color][size]) {
@@ -140,19 +171,20 @@ const Home: React.FC = () => {
         }
         else {
             console.log("product Founded : ", sortedProducts);
-            setProducts(sortedProducts);
+            setSortedProducts(sortedProducts);
         }
     }
     const orderbyMaterial = (material: string) => {
-
         var sortedProducts = products.filter(prod => prod.material === material)
-
+        if (sortedProducts.length > 0) {
+            sortedProducts.includes(sortedProducts);
+        }
         if (!sortedProducts) {
             console.log("Product Not found")
         }
         else {
             console.log("product Founded : ", sortedProducts);
-            setProducts(sortedProducts);
+            setSortedProducts(sortedProducts);
         }
     }
     const orderbyColor = (color: string) => {
@@ -164,34 +196,60 @@ const Home: React.FC = () => {
         }
         else {
             console.log("product Founded : ", sortedProducts);
-            setProducts(sortedProducts);
+            setSortedProducts(sortedProducts);
+        }
+    }
+    const orderbySize = (size: string) => {
+
+        var sortedProducts = products.filter(prod => prod.size === size)
+
+        if (!sortedProducts) {
+            console.log("Product Not found")
+        }
+        else {
+            console.log("product Founded : ", sortedProducts);
+            setSortedProducts(sortedProducts);
         }
     }
 
     return (
+
         <div className="dashboard-container">
-            <div className="search-bar">
-                <input type="text" placeholder="Search..." />
-            </div>
-            <section className="section add-trending-product">
-                <h2>Trending Products</h2>
-                <div>
+            <section>
+                <nav className="navbar">
+
+                    <ul className="navbar-links">
+                        <li><a href="/collections">Contact us</a></li>
+                        <li><a href="/new-arrivals">About</a></li>
+                    </ul>
+                    <div className="navbar-logo">
+                        <a href="/">HarishClothing</a>
+                    </div>
+                    <div className="navbar-actions">
+                        <a href="/Admin" className="navbar-action"><User /></a>
+                        <a href="/favourites" className="navbar-icon" aria-label="Favourites">
+                            <Heart />
+                        </a>
+                        <a href="/cart" className="navbar-icon" aria-label="Add to Bag">
+                            <ShoppingBag />
+                            {bagItemCount > 0 && (
+                                <span>
+                                    {bagItemCount}
+                                </span>
+                            )}
+                        </a>
+                    </div>
+                </nav>
+            </section>
+            <div className="carousel-container" style={{ backgroundColor: '#D2B48C', borderRadius: '8px 8px 8px 0' }}>
+                <h2 style={{ color: '#F5F5F5' }}>Trending Products</h2>
+                <Slider {...settings}>
                     {
-                        products.length !== 0 ? products.map((prod, index) => {
-                            if (prod.trendingProd) {
-                                return <div key={prod} style={{ border: '1px solid gray', borderRadius: '8px', margin: '5px', padding: '5px', display: 'flex', alignItems: 'center', width: '100%', maxHeight: '150px', overflow: "hidden" }}>
-                                    <div style={{ width: '30%', overflow: "hidden" }}>
-                                        <img style={{ width: '50%' }} src={`${prod.image}`} />
-                                    </div>
-                                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
-                                        {/* <h3>{(index + 1) + '. '}</h3> */}
-                                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                                            <h3 style={{ marginRight: '10px' }}>{prod.name}</h3>
-                                            <p style={{ marginRight: '10px' }}>| {prod.price + ' RS'} |</p>
-                                            <p>{prod.material} |</p>
-                                            <p>{prod.colors} | </p>
-                                        </div>
-                                    </div>
+                        products.length !== 0 ? products.map((trendprod) => {
+                            if (trendprod.trendingProd) {
+                                return <div className="carousel-slide">
+                                    <img className="carousel-image" src={`${trendprod.image}`} />
+                                    <h3 className="carousel-title">{trendprod.name}</h3>
                                 </div>
                             }
                         })
@@ -200,20 +258,21 @@ const Home: React.FC = () => {
                                 <p style={{ textAlign: 'center' }}>--- No products to show ---</p>
                             </div>
                     }
-                </div>
-            </section>
+                </Slider>
+            </div>
             <div className="dashboard-sections">
-                <section className="section add-trending-product" style={{ width: '100%' }}>
+                <section className="section add-trending-product" style={{ width: '100%', backgroundColor: '#D2B48C', borderRadius: '0 0 0 8px' }}>
                     <h2>Filter</h2>
                     <p>Materials</p>
                     <div>
                         {
                             materials.length !== 0 ? materials.map((mat) => {
                                 return <div key={mat}>
-                                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <li onClick={() => orderbyMaterial(mat)}>
+                                    <div style={{ display: 'flex', alignItems: 'center' }} key={mat}>
+                                        <label>
+                                            <input type="checkbox" value={mat} onChange={() => orderbyMaterial(mat)}></input>
                                             {mat} : {fetchCountforFiltersbyMaterial(mat)}
-                                        </li>
+                                        </label>
                                     </div>
                                 </div>
                             })
@@ -229,7 +288,10 @@ const Home: React.FC = () => {
                             sizes.length !== 0 ? sizes.map((size) => {
                                 return <div>
                                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <li>{size} : {fetchCountforFiltersbySize(size)}</li>
+                                        <label>
+                                            <input type="checkbox" value={size} onChange={() => orderbySize(size)}></input>
+                                            {size} : {fetchCountforFiltersbySize(size)}
+                                        </label>
                                     </div>
                                 </div>
                             })
@@ -245,9 +307,10 @@ const Home: React.FC = () => {
                             prices.length !== 0 ? prices.map((price) => {
                                 return <div>
                                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <li onClick={() => orderbyPrice(price)}>
+                                        <label>
+                                            <input type="checkbox" value={price} onChange={() => orderbyPrice(price)}></input>
                                             {price} : {fetchCountforFiltersbyPrice(price)}
-                                        </li>
+                                        </label>
                                     </div>
                                 </div>
                             })
@@ -263,7 +326,10 @@ const Home: React.FC = () => {
                             colors.length !== 0 ? colors.map((color) => {
                                 return <div>
                                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <li onClick={() => { orderbyColor(color) }}>{color} : {fetchCountforFiltersbyColor(color)} </li>
+                                        <label>
+                                            <input type="checkbox" value={color} onChange={() => orderbyColor(color)}></input>
+                                            {color} : {fetchCountforFiltersbyColor(color)}
+                                        </label>
                                     </div>
                                 </div>
                             })
@@ -276,20 +342,17 @@ const Home: React.FC = () => {
                 </section>
                 <section className="section add-trending-product" style={{ width: '96%' }}>
                     <h2>Products</h2>
-                    <div>
+                    <div className="grid-container">
                         {
-                            products.length !== 0 ? products.map((prod, index) => {
-                                return <div key={prod} style={{ border: '1px solid gray', borderRadius: '8px', margin: '5px', padding: '5px', display: 'flex', alignItems: 'center', width: '100%', maxHeight: '150px', overflow: "hidden" }}>
-                                    <div style={{ width: '30%', overflow: "hidden" }}>
-                                        <img style={{ width: '100%', height: '100%' }} src={`${prod.image}`} />
+                            sortedProducts.length !== 0 ? sortedProducts.map((prod) => {
+                                return <div className="product-card">
+                                    <div>
+                                        <img className="product-image" src={`${prod.image}`} />
                                     </div>
-                                    <div style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
-                                        <h3>{(index + 1) + '. '}</h3>
-                                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                                            <h3 style={{ marginRight: '10px' }}>{prod.name}</h3>
-                                            <p style={{ marginRight: '10px' }}>| {prod.price + ' RS'} |</p>
-                                            <p>{prod.material} |</p>
-                                            <p>{prod.colors}</p>
+                                    <div>
+                                        <div>
+                                            <p className="product-name">{prod.name}</p>
+                                            <p className="product-price">{prod.price + ' RS'}</p>
                                         </div>
                                     </div>
 
